0c5537d52c3c4d110ec9625451e7573aabfbeebb
Tented <tented@protonmail.com>
2023-01-09 19:57:40 +0100

    Porject files

---
+++ StatFetcher/Program.cs
@@ -0,0 +1,11 @@
+﻿using StatFetcher;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        var statScraper = new StatScraper();
+        statScraper.GetStats();
+    }
+}
---
+++ StatFetcher/StatFetcher.cs
@@ -0,0 +1,26 @@
+﻿using GemBox.Spreadsheet;
+using System.Xml.Linq;
+
+
+namespace StatFetcher
+{
+    public class StatScraper
+    {
+        private const string url = "https://plk.pl/statystyki/xml/1";
+        public void GetStats()
+        {
+            var filePath = "/var/www/html/bstattable";
+            var path = $"{filePath}/index.html";
+            var xml = XDocument.Load(url).Document?.ToString();
+            if (xml is null)
+            {
+                throw new NullReferenceException("There was no file to download. Possible PLK website problem");
+            }
+
+            var result = xml.Substring(xml.IndexOf("<table"));
+            result = result.Remove(result.LastIndexOf("</table>") + 8);
+
+            File.WriteAllText(path, result);
+        }
+    }
+}
